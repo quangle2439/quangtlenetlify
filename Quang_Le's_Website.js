2a5360f28817bbdf1cc92b902e661b7110c7e92c
Quang T Le <92972399+quangle2439@users.noreply.github.com>
2023-07-09 17:49:50 -0700

    Add files via upload

---
+++ Quang_Le's_Website.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button1");
+button.addEventListener("click", buttonAction);
+
+
+function buttonAction(){
+  window.open("https://www.youtube.com/");
+  window.location="buttons.html";
+}
